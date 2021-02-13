3e5f1984c6cd280bbe359d6494db701b10d80450
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-02-13 12:17:50 +0100

    Create merged_string_checker.py

---
+++ 5 Kyu/merged_string_checker.py
@@ -0,0 +1,4 @@
+def is_merge(s, part1, part2):
+    return (s == part1 + part2 if not (s and part1 and part2) else
+        s[0] == part1[0] and is_merge(s[1:], part1[1:], part2) or
+        s[0] == part2[0] and is_merge(s[1:], part1, part2[1:]))
