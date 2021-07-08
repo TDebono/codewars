46b482672698c1c8f52daf3256a1fbc1973e790d
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-07-08 15:35:14 +0200

    Create middleChar.java

---
+++ 8 Kyu/middleChar.java
@@ -0,0 +1,19 @@
+class Kata {
+  public static String getMiddle(String word) {
+    String s = "";
+    int length = word.length();
+    int half = length/2;
+
+    if (length % 2 == 0) {
+      
+      s = word.substring(half - 1, half + 1);
+      
+    } else {
+      
+      s = word.substring(half, half + 1);
+    
+    }
+    
+    return s;
+  }
+}
