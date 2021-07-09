a6d13d2bb1e45c3dd592a7c7388555d17f3e158b
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-07-09 18:13:08 +0200

    Create greetings.java

---
+++ 8 Kyu/greetings.java
@@ -0,0 +1,8 @@
+public class Kata
+{
+  public static String greet(String name)
+  {
+//     System.out.println("Hello, " + name + " how are you doing today?");
+    return "Hello, " + name + " how are you doing today?";
+  }
+}
