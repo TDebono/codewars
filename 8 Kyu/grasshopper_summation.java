a3a5a37a204bf48466da5db01c8cedee772c203d
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-07-16 15:52:40 +0200

    Create grasshopper_summation.java

---
+++ 8 Kyu/grasshopper_summation.java
@@ -0,0 +1,22 @@
+public class GrassHopper {
+  
+  """
+    return n * (n+1) / 2;
+    
+  """
+
+    public static int summation(int n) {
+      
+      int sum = 0;
+      
+      for (int i = 0; i <= n; i++) {
+        
+        sum += i;
+        
+      }
+      
+      return sum;
+      
+      
+    }
+}
