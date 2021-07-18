d23d691e629dc735ddc8257c751b4ccb026355b3
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-07-18 14:18:54 +0200

    Create needle2.java

---
+++ 8 Kyu/needle2.java
@@ -0,0 +1,8 @@
+public class Kata {
+  public static String findNeedle(Object[] haystack) {
+    
+    return String.format("found the needle at position %d", java.util.Arrays.asList(haystack).indexOf("needle"));
+    
+  }
+  
+}
